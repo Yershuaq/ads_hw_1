2cb18aa313478288141f0d70fefcecef0e780602
solta <230930@astanait.edu.kz>
2024-04-07 23:18:06 +0500

    added number_5

---
+++ src/number_6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class number_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the base (a): ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the exponent (n): ");
+        int n = scanner.nextInt();
+
+        int result = calculatePower(a, n);
+
+        System.out.println(a + "^" + n + " = " + result);
+
+        scanner.close();
+    }
+
+    public static int calculatePower(int a, int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return a * calculatePower(a, n - 1);
+        }
+    }
+}
