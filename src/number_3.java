2c5a746111b12713520413b6a0e2069e3d951986
solta <230930@astanait.edu.kz>
2024-04-07 22:01:22 +0500

    added number_2

---
+++ src/number_3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class number_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        boolean isPrime = isPrime(n);
+
+        if (isPrime) {
+            System.out.println(n + " is Prime");
+        } else {
+            System.out.println(n + " is Composite");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
